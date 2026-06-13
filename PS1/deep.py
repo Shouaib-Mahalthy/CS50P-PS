053eaf97fd12eeee9fb732f13b10996d88721440
Shouaib Mahalthy <mahalthyshouaib@gmail.com>
2026-06-13 22:18:12 +0530

    Update deep.py

--- PS1/deep.py
+++ PS1/deep.py
@@ -1,4 +1,4 @@
-inp = input("What is the Answer to the Great Question of Life,the Universe, and Everything?").strip().lower()
+inp = input("What is the Answer to the Great Question of Life,the Universe, and Everything? ").strip().lower()
 if inp == "42":
   print("Yes")
 elif inp == "forty two":
