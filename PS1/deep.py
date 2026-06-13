b057727e3337be9c7d3846fd975ec5a76d1a4d56
Shouaib Mahalthy <mahalthyshouaib@gmail.com>
2026-06-13 22:17:18 +0530

    Add input check for the answer to life question

---
+++ PS1/deep.py
@@ -0,0 +1,10 @@
+inp = input("What is the Answer to the Great Question of Life,the Universe, and Everything?").strip().lower()
+if inp == "42":
+  print("Yes")
+elif inp == "forty two":
+  print("Yes")
+elif inp == "forty-two":
+  print("Yes")
+else:
+  print("No")
+
