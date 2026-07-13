e929bffb0ebf79cdb667a7f95fb96d7d86cad6ba
Shouaib Mahalthy <mahalthyshouaib@gmail.com>
2026-07-13 22:22:28 +0530

    Implement file type detection in extensions.py
    Add file type detection for various extensions.

---
+++ PS1/extensions.py
@@ -0,0 +1,15 @@
+inp = input().strip().lower()
+if inp.endswith(".jpg") or inp.endswith(".jpeg"):
+    print("image/jpeg")
+elif inp.endswith(".gif"):
+    print("image/gif")
+elif inp.endswith(".png"):
+    print("image/png")
+elif inp.endswith(".pdf"):
+    print("application/pdf")
+elif inp.endswith(".txt"):
+    print("text/plain")
+elif inp.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
